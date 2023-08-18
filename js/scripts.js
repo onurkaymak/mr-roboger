307125bb2c1e6307a60775f4dce1ee37307e2236
onurkaymak <onurkaymak@outlook.com>
2023-08-18 09:14:30 -0700

    Add askMrRoboger function Test#1, passing.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,12 @@
 // Business Logic
 
+function askMrRoboger(userNum) {
+    const inputArr = [];
+    for (let i = 1; i <= userNum; i += 1) {
+        inputArr.push(i);
+    }
+    console.log(inputArr)
+};
+
+
+askMrRoboger(10);
